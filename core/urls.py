05129201853fef16b21eb849f717ef045614ae26
Celso Reis <celsosr87@gmail.com>
2025-07-29 23:27:03 -0300

    Desenvolvimento da função de cadastro e edição de pascientes

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.home, name='home'),
+
+# CRUD Paciente
+    path('pacientes/', views.PacienteListView.as_view(), name='paciente-list'),
+    path('pacientes/novo/', views.PacienteCreateView.as_view(), name='paciente-create'),
+    path('pacientes/<int:pk>/editar/', views.PacienteUpdateView.as_view(), name='paciente-update'),
+    path('pacientes/<int:pk>/excluir/', views.PacienteDeleteView.as_view(), name='paciente-delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,43 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Paciente
 
-@login_required
+# Home protegida
 def home(request):
     return render(request, 'core/home.html')
+
+# Login / Logout
+class LoginUsuario(LoginView):
+    template_name = 'core/login.html'
+
+class LogoutUsuario(LogoutView):
+    next_page = 'login'
+
+# Lista
+class PacienteListView(LoginRequiredMixin, ListView):
+    model = Paciente
+    template_name = 'core/pacientes/list.html'
+    context_object_name = 'pacientes'
+
+# Criar
+class PacienteCreateView(LoginRequiredMixin, CreateView):
+    model = Paciente
+    fields = ['nome', 'data_nascimento', 'cpf', 'telefone', 'email']
+    template_name = 'core/pacientes/form.html'
+    success_url = reverse_lazy('paciente-list')
+
+# Atualizar
+class PacienteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Paciente
+    fields = ['nome', 'data_nascimento', 'cpf', 'telefone', 'email']
+    template_name = 'core/pacientes/form.html'
+    success_url = reverse_lazy('paciente-list')
+
+# Excluir
+class PacienteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Paciente
+    template_name = 'core/pacientes/confirm_delete.html'
+    success_url = reverse_lazy('paciente-list')
