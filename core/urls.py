4032be0767bfa01819726bd692670f74d1398a08
Celso Reis <celsosr87@gmail.com>
2025-08-03 23:31:05 -0300

    Desenvolvimento do layout de cadastro de pacientes

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path('', LoginView.as_view(template_name='core/login.html'), name='login'),
     path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('home', views.home, name='home'),
