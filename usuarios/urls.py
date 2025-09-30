b1c1c326d34a73272cbd61f4777b5433ab0adf74
Celso Reis <celsosr87@gmail.com>
2025-09-30 11:38:35 -0300

    Funcionalidade de Logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("", index, name="index"),
 ]
